e6f095ff549f9576c46dbd015b733a25b8fe7405
jma2191 <31739589+jma2191@users.noreply.github.com>
2022-10-14 12:17:59 -0600

    created home page sql, created empty inc.php

--- includes/database-helper.php
+++ includes/database-helper.php
@@ -31,7 +31,7 @@ class DatabaseHelper{
 
 class SongDB{
 
-    private static $sql = "SELECT title, artist_name as name, type_name as type, genre_name as genre, year, duration, bpm, energy, danceability, liveness, valence, acousticness, speechiness, popularity, songs.artist_id, artist_type_id, songs.genre_id, song_id
+    private static $sqlSearch = "SELECT title, artist_name as name, type_name as type, genre_name as genre, year, duration, bpm, energy, danceability, liveness, valence, acousticness, speechiness, popularity, songs.artist_id, artist_type_id, songs.genre_id, song_id
     FROM songs
     LEFT JOIN artists ON artists.artist_id=songs.artist_id
     LEFT JOIN genres ON genres.genre_id=songs.genre_id
@@ -42,56 +42,122 @@ class SongDB{
     }
 
     public function getSingleSong($songID){
-        $sql = self::$sql. ' WHERE song_id ='. $songID; //may need to be change to song id
+        $sql = self::$sqlSearch. ' WHERE song_id ='. $songID; //may need to be change to song id
         $results = DatabaseHelper::runQuery($this->pdo, $sql);
         return $results -> fetchAll();
     }
 
+
+    /*** SEARCH FUNCTIONS ***/
     public function searchByAll(){
-        $sql = self::$sql;
+        $sql = self::$sqlSearch;
         $results = DatabaseHelper::runQuery($this->pdo, $sql);
         return $results -> fetchAll();
     }
 
     public function searchByTitle($title){
-        $sql = self::$sql." WHERE title LIKE :songTitle"; //near match because user can search partial
+        $sql = self::$sqlSearch." WHERE title LIKE :songTitle"; //near match because user can search partial
         $results = DatabaseHelper::runQuerySinglePrepare($this->pdo, $sql, "%$title%", ':songTitle');
         return $results -> fetchAll();
     }
 
     public function searchByYearGreater($greater){
-        $sql = self::$sql." WHERE year >= $greater";
+        $sql = self::$sqlSearch." WHERE year >= $greater";
         $results = DatabaseHelper::runQuery($this->pdo, $sql);
         return $results -> fetchAll();
     }
 
     public function searchByYearLesser($less){
-        $sql = self::$sql." WHERE year <= $less";
+        $sql = self::$sqlSearch." WHERE year <= $less";
         $results = DatabaseHelper::runQuery($this->pdo, $sql);
         return $results -> fetchAll();
     }
 
     public function searchByPopularityGreater($greater){
-        $sql = self::$sql ." WHERE popularity >= $greater";
+        $sql = self::$sqlSearch ." WHERE popularity >= $greater";
         $results = DatabaseHelper::runQuery($this->pdo, $sql);
         return $results -> fetchAll();
     }
 
     public function searchByPopularityLesser($less){
-        $sql = self::$sql ." WHERE popularity <= $less";
+        $sql = self::$sqlSearch ." WHERE popularity <= $less";
         $results = DatabaseHelper::runQuery($this->pdo, $sql);
         return $results -> fetchAll();
     }
 
     public function searchByArtist($artist){
-        $sql = self::$sql." WHERE artist_name = :artist";
+        $sql = self::$sqlSearch." WHERE artist_name = :artist";
         $results = DatabaseHelper::runQuerySinglePrepare($this->pdo, $sql, $artist, ':artist');
         return $results -> fetchAll();
     }
 
     public function searchByGenre($genre){
-        $sql = self::$sql." WHERE genre_name = :genre";
+        $sql = self::$sqlSearch." WHERE genre_name = :genre";
         $results = DatabaseHelper::runQuerySinglePrepare($this->pdo, $sql, $genre, ':genre');
+        return $results -> fetchAll();
+    }
+
+    /***TOP 10 FUNCTIONS***/
+    public function getTopPopularSongs(){
+        $sql = "SELECT popularity, artist_name as name, title, songs.artist_id
+        FROM songs
+        INNER JOIN artists
+        ON artists.artist_id = songs.artist_id
+        GROUP BY title 
+        ORDER BY popularity DESC LIMIT 10";
+        $results = DatabaseHelper::runBasicQuery($this->pdo, $sql);
+        return $results -> fetchAll();
+    }
+
+    public function getTopOneHitsSongs(){
+        $sql = "SELECT artist_name as name, title, COUNT(title) as numSongs, popularity, songs.artist_id
+        FROM songs
+        INNER JOIN artists
+        ON artists.artist_id = songs.artist_id
+        GROUP BY artist_name 
+        HAVING COUNT(title) = 1
+        ORDER BY popularity DESC LIMIT 10";
+        $results = DatabaseHelper::runBasicQuery($this->pdo, $sql);
+        return $results -> fetchAll();
+    }
+
+    public function getTopAcousticSongs(){
+        $sql = "SELECT title, acousticness, duration
+        FROM songs
+        GROUP BY title 
+        HAVING acousticness > 40
+        ORDER BY duration DESC LIMIT 10";
+        $results = DatabaseHelper::runBasicQuery($this->pdo, $sql);
+        return $results -> fetchAll();
+    }
+
+    public function getTopClubSongs(){
+        $sql = "SELECT title, danceability 
+        FROM songs
+        GROUP BY title 
+        HAVING danceability > 80
+        ORDER BY ((danceability*1.6)+(energy*1.4)) DESC LIMIT 10";
+        $results = DatabaseHelper::runBasicQuery($this->pdo, $sql);
+        return $results -> fetchAll();
+    }
+
+    public function getTopRunningSongs(){
+        $sql = "SELECT title, bpm, speechiness
+        FROM songs
+        GROUP BY title 
+        HAVING bpm > 100 AND bpm < 115
+        ORDER BY ((energy*1.3)+(valence*1.6)) DESC LIMIT 10";
+        $results = DatabaseHelper::runBasicQuery($this->pdo, $sql);
+        return $results -> fetchAll();
+    }
+
+    public function getTopStudySongs(){
+        $sql = "SELECT title, bpm, speechiness
+        FROM songs
+        GROUP BY title 
+        HAVING (bpm > 100 AND bpm < 115) AND (speechiness > 1 AND speechiness < 20)
+        ORDER BY ((acousticness*0.8)+(100-speechiness)+(100-valence)) DESC LIMIT 10";
+        $results = DatabaseHelper::runBasicQuery($this->pdo, $sql);
         return $results -> fetchAll();
     }
 
@@ -107,6 +173,17 @@ class ArtistDB{
         $results = DatabaseHelper::runBasicQuery($this->pdo,$sql);
         return $results -> fetchAll();
     }
+    
+    public function getTopArtists(){
+        $sql = "SELECT artist_name as name, COUNT(title) as numSongs, songs.artist_id
+        FROM artists
+        INNER JOIN songs
+        ON songs.artist_id = artists.artist_id
+        GROUP BY artist_name 
+        ORDER BY Count(title) DESC LIMIT 10";
+        $results = DatabaseHelper::runBasicQuery($this->pdo, $sql);
+        return $results -> fetchAll();
+    }
 }
 
 class GenreDB{
@@ -117,6 +194,17 @@ class GenreDB{
     public function getGenres(){
         $sql = "SELECT genre_name as genre FROM genres";
         $results = DatabaseHelper::runBasicQuery($this->pdo,$sql);
+        return $results -> fetchAll();
+    }
+
+    public function getTopGenres(){
+        $sql = "SELECT genre, COUNT(title) as numSongs, genres.genre_id
+        FROM genres
+        INNER JOIN songs
+        ON songs.genre_id = genres.genre_id
+        GROUP BY genre 
+        ORDER BY COUNT(title) DESC LIMIT 10";
+        $results = DatabaseHelper::runBasicQuery($this->pdo, $sql);
         return $results -> fetchAll();
     }
 }
